2043d3fa6a2ae4709575650d1d10b4ec8e4a415a
Kane Bruce <kbruce230@gmail.com>
2022-12-12 20:09:47 -0800

    Add runtime wing pivot add/remove functions

--- Runtime/Oku/Utils/Controllers/SwingWingController.cs
+++ Runtime/Oku/Utils/Controllers/SwingWingController.cs
@@ -25,7 +25,7 @@ namespace Oku.Utils.Controllers
 
 		private readonly Dictionary<string, UnityAction<string, float>> _activeHooks =
 			new Dictionary<string, UnityAction<string, float>>();
-
+		
 		private void OnEnable()
 		{
 			wingPivots.ForEach(wing =>
@@ -51,5 +51,34 @@ namespace Oku.Utils.Controllers
 				setPivot -= _activeHooks[wing.id];
 			});
 		}
+
+        public void AddWingPivot(SwingWingParams wing)
+        {
+            wingPivots.Add(wing);
+            if (!enabled)
+                return;
+            if (!_activeHooks.ContainsKey(wing.id))
+            {
+                UnityAction<string, float> action = (id, extendParam) =>
+                {
+                    if (id == null || !id.Equals(wing.id))
+                        return;
+                    var newRot = Vector3.Lerp(wing.baseRotation, wing.maxRotation, extendParam);
+                    wing.pivot.transform.localRotation = Quaternion.Euler(newRot);
+                };
+                _activeHooks.Add(wing.id, action);
+            }
+            setPivot += _activeHooks[wing.id];
+		}
+
+        public void RemoveWingPivot(string wingId)
+        {
+            if (!_activeHooks.ContainsKey(wingId))
+                return;
+			if (enabled)
+                setPivot -= _activeHooks[wingId];
+            
+            _activeHooks.Remove(wingId);
+        }
 	}
 }
